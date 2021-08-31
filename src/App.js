8a60f3e611e37e2677af79e93ca0821588beef13
belaguarino <izabelaguarino@gmail.com>
2021-08-31 20:12:48 -0300

    Passo 3 e 4 com add no App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
-      {/* */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <Header />
+      <MovieList movies={ movies } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
-// implement MovieList component here
+import React from 'react';
+
+// Passo 4
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        {/* Passo 5: fazer dinâmico com a Ley sendo o valor único de nome. */}
+        {movies.map((mov) => (
+          <MovieCard key={ mov.title } movie={ mov } />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
