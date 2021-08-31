df9678616fd42215b5c20ac9982c0e18e2a692a5
belaguarino <izabelaguarino@gmail.com>
2021-08-31 20:20:05 -0300

    Refação dos passos 3 e 4 com o Movie Card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
-// implement MovieCard component here
+// Passo 3
+import React from 'react';
+
+
+class MovieCard extends React.Component {
+  render() {
+    // 3) Receber o movies como prop. Fazer fora do return!
+    return (
+    // Passo 6
+      <div>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
