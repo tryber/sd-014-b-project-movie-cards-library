364cf3e867908ca9dc248c1ae39ec666fc3fdcc0
belaguarino <izabelaguarino@gmail.com>
2021-08-31 20:24:22 -0300

    Conteúdo do rating, passos 11 e 12

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
-// implement Rating component here
+// Passo 11 e 12
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <p className="rating">{rating}</p>
+    );
+  }
+}
+
+export default Rating;
