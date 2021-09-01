077afc175dda81a4d5c96cdeb1c602e1d0b30c1d
Riba Jr / RSTD <rstd.creative@gmail.com>
2021-09-01 20:44:50 -0300

    Modify styles

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,7 +12,7 @@ class MovieCard extends React.Component {
           <img className="movie-card-image" src={ imagePath } alt={ title } />
           <h4 className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
-          <p className="">{ storyline }</p>
+          <p className="movie-card-storyline">{ storyline }</p>
           <Rating rating={ rating } />
         </div>
       </section>
