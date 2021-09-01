9c31f28884e2447a4f81312d96d194c010be9187
Elithon Silva <elithonsilva@gmail.com>
2021-09-01 18:02:23 -0300

    impl: PropTypes no componente <Rating />

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,6 @@
 // implement Rating component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
@@ -9,5 +10,11 @@ class Rating extends React.Component {
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.shape({
+    rating: PropTypes.number,
+  }).isRequired,
+};
 
 export default Rating;
