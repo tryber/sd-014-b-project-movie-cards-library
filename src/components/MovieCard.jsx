a70015f6d168a648da773d4806507cdee80aa2b4
Nataniel Santos <nathan.santos159@hotmail.com>
2021-09-01 17:56:34 -0300

    Requisito 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath, rating } = this.props
+    const { title, subtitle, storyline, imagePath, rating } = this.props;
+    return (
+      <img src={ imagePath } alt={ `Imagem do filme ${title}` } />
+    );
   }
 }
 
