cba04b04dc161b3bde1d99afc741848cf42554f1
vanim <vanimouzad123@gmail.com>
2021-09-01 16:24:34 -0300

    Requisito 11 ao 14 - Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,10 @@
 // implement MovieCard component here
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
 
     return (
       <section>
@@ -11,6 +12,7 @@ class MovieCard extends React.Component {
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
+        <Rating rating={ rating } />
       </section>
     );
   }
