704cf698b6fd52dc11af66dfbeb9be7d5e144fd0
Arturo Vaine <arturo.vaine@gmail.com>
2021-09-01 15:17:49 -0300

    Requisito MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Movie list</p>
+      </div>
+    );
+  }
+}
+
+export default MovieList;
