cc98c21f2f48eb7d91dd5f4d9036bce7c81e1218
icaroemanuel <icaro.emanuel@outlook.com>
2021-09-01 16:29:13 -0300

    Requisito 3 - componente MovieCard e correçoes do lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <section>
+        {movies}
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
