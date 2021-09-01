49260fafa68695c47bc35fb14020a88cf1ac6f6e
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-09-01 16:02:53 -0300

    Ja nem sei que requisito to fazendo atendeu um monte Oo

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,28 @@
 // implement MovieCard component here
+import React from 'react';
+import propTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    // Desestruturação abaixo feita dessa forma por conta do lint
+    // Estava - const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    return (
+      <section>
+        <img src={ imagePath } alt={ `${title}` } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+      </section>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  title: propTypes.string,
+  subtitle: propTypes.string,
+  storyline: propTypes.string,
+  imagePath: propTypes.string,
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+import Movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      Movies.map((eachMovie, index) => <MovieCard key={ index } movies={ eachMovie }/>)
+    )
+  }
+}
+
+  
+export default MovieList;
