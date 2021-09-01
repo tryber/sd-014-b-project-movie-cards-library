01a15646ecc02f96f256fa94f968731b1e36291d
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-09-01 19:05:04 -0300

    colocando as tags pedidas no projeto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import movies from '../data';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const {movie:{title, subtitle, storyline, imagePath, rating}} = this.props;
     return (
       <div>
-        <h2>{title}</h2>
-        <h3>{subtitle}</h3>
-        <p>{storyline}</p>
         <img src={ imagePath } alt="capa do filme" />
-        <p>{rating}</p>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating />
       </div>);
   }
 }
 
 MovieCard.propType = {
   movies: PropTypes.arrayOf,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+  imagePath: PropTypes.string,
+  rating: PropTypes.string,
 };
 MovieCard.defaultProps = {
   movies: 'Faltou filmes',
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import movies from '../data';
 
 class Rating extends React.Component {
   render() {
-    return <div>{forca}</div>;
+    return <div>{movies.rating}</div>;
   }
 }
 
