46a7f87cff8ec429e9610aee1dda9704f99309d5
Ronaldo Junior <ronaldosenajr@gmail.com>
2021-09-01 15:46:28 -0400

    Realizado o requisito 9

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,18 +8,19 @@ class MovieCard extends React.Component {
       <div>
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
       </div>
     );
   }
 }
 
-// MovieCard.propTypes = {
-//   movies: PropTypes.arrayOf(PropTypes.object).isRequired,
-//   title: PropTypes.string.isRequired,
-//   subtitle: PropTypes.string.isRequired,
-//   storyline: PropTypes.string.isRequired,
-//   rating: PropTypes.number.isRequired,
-//   imagePath: PropTypes.string.isRequired,
-// };
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.object).isRequired,
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  imagePath: PropTypes.string.isRequired,
+};
 
 export default MovieCard;
