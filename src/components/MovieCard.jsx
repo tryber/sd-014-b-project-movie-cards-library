ccde56bcfb147cfc8fbf49e6ed477c451672268b
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-09-01 01:21:15 -0300

    Requisito 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,10 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,15 @@
 // implement MovieList component here
 import React, { Component } from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
-    
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((movie) => <MovieCard key="" movie={ movie } />)}
+      </div>
+    );
   }
 }
 
