ed734d5b18118e50b0f250e5eae70406dfbf31aa
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-09-01 17:44:48 -0400

    Requisito 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    // const { movies } = this.props;
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
-      <section><h2>Vamos Começar</h2></section>
+      <section>
+        <main>
+          <img src={ imagePath } alt={ title } />
+          <h2>{title}</h2>
+          <p>{subtitle}</p>
+          <p>{storyline}</p>
+          <h2>{rating}</h2>
+        </main>
+      </section>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.string.isRequired,
+};
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <section>{ movies }</section>
+      <section>
+        {movies.map((filme, index) => <MovieCard key={ index } movie={ filme } />)}
+      </section>
     );
   }
 }
