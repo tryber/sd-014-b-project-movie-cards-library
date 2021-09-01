c7b8714d20fb39bc40fe5bda06ddd0dd9b13f228
LucasAccurcio <lucas.accurcio@gmail.com>
2021-09-01 16:19:34 -0300

    Requisito 04 e 05

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,30 @@
 // implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    return (
+      <div className="movie-card">
+        <img src={ imagePath } alt={ `Imagem filme ${title}` } />
+        <p>{ title }</p>
+        <p>{ subtitle }</p>
+        <p>{ storyline }</p>
+        <p>{ rating }</p>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,20 @@
 // implement MovieList component here
 import React from 'react';
+// import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return <p>Algo</p>;
+    const { movies } = this.props;
+    return (
+      <div className="movies-list">
+        {movies
+          .map((movie, index) => {
+            const chave = `Movie Title ${index + 1}`;
+            return <MovieCard key={ chave } movie={ movie } />;
+          }, [])}
+      </div>
+    );
   }
 }
 
