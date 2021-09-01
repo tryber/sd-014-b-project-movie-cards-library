bc7788ae76a194bd0e25bfd51eb462b4e14c8654
Nataniel Santos <nathan.santos159@hotmail.com>
2021-09-01 17:48:55 -0300

    Requisitos 4 5 e 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,9 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const {movies} = this.props;
+    const { movies } = this.props;
+    return (movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />));
   }
 }
 
