a55267e77c0a6b8b70428bcb7c568ef0dfbc96e8
icaroemanuel <icaro.emanuel@outlook.com>
2021-09-01 19:38:13 -0300

    Reqs 15,16 e 17 - Renderizando o app com a lista

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,12 +5,13 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { 
-      movie: {title, subtitle, storyline, imagePath, rating} 
+      movie: { title, subtitle, storyline, imagePath, rating } 
     } = this.props;
 
     return (
       <section>
-        <img src={imagePath} alt={title} />
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
       </section>
     );
   }
