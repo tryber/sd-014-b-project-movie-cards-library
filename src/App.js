0c4b667d05b2d300a96197ffbc81893c8205e0cc
Guilherme <guimfontes7@gmail.com>
2021-09-01 16:17:27 -0300

    Cria componentes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.css';
+import PropTypes from 'prop-types';
 import Header from './components/Header';
+import movies from './data';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ movies } />
     </div>
   );
 }
 
+movies.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
 export default App;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,29 @@
 // implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props.movies
+    return (
+      <main className="MovieList">
+        {movies.map((movie) => <MovieCard movie={ movie } />)}
+      </main>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+export default MovieList;
