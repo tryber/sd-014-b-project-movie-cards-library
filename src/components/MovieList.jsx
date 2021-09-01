a1b933277da2aa6ae27d8003597556caf343c1de
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-09-01 18:42:43 -0300

    Requesito numero #3 corrigido.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,22 @@
 // implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      { movies }
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.string.isRequired,
+};
+
+/* MovieList.defaultProps = {
+  movies: [],
+}; */
+
+export default MovieList;
