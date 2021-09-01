3e1e1fcf137391a6720a9b3dea010f88efac2329
Cassio Mascarenhas <csmlvx@gmail.com>
2021-09-01 17:13:12 -0300

    Commiton Bakudan

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
-import data from './data';
+import movies from './data';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <MovieList Movies={ data } />
+        <MovieList movies={ movies } />
       </div>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
@@ -7,5 +8,17 @@ class MovieList extends React.Component {
     return movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape(
+      {
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string.isRequired,
+        storyline: PropTypes.isRequired,
+      },
+    ),
+  ).isRequired,
+};
 
 export default MovieList;
