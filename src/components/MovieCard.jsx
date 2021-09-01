763480fcf4d06cb71a86984861707638885d2ee4
Lombeira <glomenha@gmail.com>
2021-09-01 17:49:51 -0300

    Quem sou eu sem silveira ?

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,12 +10,12 @@ class MovieCard extends Component {
     } = this.props;
 
     return (
-      <section>
-        <img src={ imagePath } alt={ title } />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        <Rating rating={ rating } />
+      <section className="movie-card movie-card-body">
+        <img className="movie-card-image" src={ imagePath } alt={ title } />
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
+        <Rating className="movie-card-rating" rating={ rating } />
       </section>
     );
   }
