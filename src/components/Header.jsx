a9687eba10674aa577ceba285ee767cb3e20bd4c
Mariana Ferreira <marianadesouzaferreira01@gmail.com>
2021-09-01 17:19:40 -0300

    Requisito 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <Header />
+      <header>
+        <Header />
+        <h1>Movie Cards Library</h1>
+      </header>
     );
   }
 }
