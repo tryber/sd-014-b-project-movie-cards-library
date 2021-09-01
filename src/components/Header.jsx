490da70a5070d6a2b4b192bf8423769a9b140424
eliHC <leonardohcds@gmail.com>
2021-09-01 17:15:19 -0300

    rebranched

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,18 @@
-// implement Header component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+
+};
+
+export default Header;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,24 @@
-// implement MovieList component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+const mapAndRender = (arr) => arr
+  .map((element) => <MovieCard movies={ element } key={ element.title } />);
+
+class MovieList extends React.Component {
+  render() {
+    // const { movies: { title, subtitle, storyline, rating, imagePath } } = this.props;
+
+    return (
+      <main>
+        { mapAndRender(movies) }
+      </main>
+    );
+  }
+}
+
+MovieList.propTypes = {
+
+};
+
+export default MovieList;
