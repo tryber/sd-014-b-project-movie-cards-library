6ebf787d70d38c9f4d1a26d853467cdec0f2d2ae
Cecilia Cacais <ceci.cacais@gmail.com>
2021-09-01 16:38:14 -0300

    Iniciando o MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    const { filmes } = this.props;
+    return (
+      <section>{ filmes }</section>
+    );
+  }
+}
+MovieList.propTypes = {
+
+}.isRequired;
+
+export default MovieList;
