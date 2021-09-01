b84b16e67151918084e24649446c19a08eb850a0
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-09-01 17:10:33 -0300

    Rating completo

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,10 @@
 // implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    return (<h1 className="rating">{this.props.rating}</h1>);    
+  }
+}
+
+export default Rating;
