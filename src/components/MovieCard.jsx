11cd639c4f1ffecb6ad08c7805c76505c77a4697
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-09-01 20:19:55 -0300

    Fechando o css padrao do exemplo no projeto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,11 +9,13 @@ class MovieCard extends React.Component {
     // Estava - const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     const { movie: { title, subtitle, rating, storyline, imagePath } } = this.props;
     return (
-      <section className="movie-card movie-card-body">
+      <section className="movie-card">
         <img className="movie-card-image" src={ imagePath } alt={ `${title}` } />
-        <h4 className="movie-card-title">{ title }</h4>
-        <h5 className="movie-card-subtitle ">{ subtitle }</h5>
-        <p className="movie-card-storyline ">{ storyline }</p>
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle ">{ subtitle }</h5>
+          <p className="movie-card-storyline ">{ storyline }</p>
+        </div>
         <section className="movie-card-rating">
           <Rating rating={ rating } />
         </section>
