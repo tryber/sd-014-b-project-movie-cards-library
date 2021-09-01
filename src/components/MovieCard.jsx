a5c5067e469d8558ebef8ab2e9f4f73211d52fc6
João Reis <joaoreisjpk@gmail.com>
2021-09-01 19:10:52 -0300

    eslint ?

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,8 +5,15 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating} = this.props.movie;
-    const {}
+    const { key } = this.props
 
+    return (
+      <section>
+        <img src={ imagePath } />
+        <h4>{ title }</h4>
+      </section>
+      
+    )
   }
 }
 
