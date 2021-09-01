ed9c6f8e8aecfa91ff8eed9ab404fbd9cc7ccba4
jarana-2020 <julioaranabr@yahoo.com.br>
2021-09-01 17:10:52 -0300

    Refatorado requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
+import data from '../data';
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
     return (
       <div>
         <div>
-          {movies.map((actualMovie) => (<MovieCard
-            key={ movie.title }
-            movie={ actualMovie }
-          />))}
+          {data.map((movie) => (<MovieCard key={ movie.title } movie={ movie } />))}
         </div>
       </div>
     );
   }
 }
 
-MovieList.propTypes = {
-  movies: PropTypes.arrayOf(propTypes.object).isRequired,
-};
 export default MovieList;
