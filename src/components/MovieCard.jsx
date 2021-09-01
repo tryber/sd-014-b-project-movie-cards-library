f16ac171ea6f2b7a404f95e00cf2f77bc9ec7d57
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-09-01 01:32:52 -0300

    Requisito 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 
 class MovieCard extends Component {
   render() {
-    // const { movie } = this.props;
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
-      <div />
+      <div>
+        <img src={ imagePath } alt={ `Imagem do filme ${title}`} />
+      </div>
     );
 
   }
