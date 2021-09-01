108699bb8f9bfa968637232241945105bd710283
Victoria Lorena <victorialorenaf.gomes@gmail.com>
2021-09-01 19:06:38 -0300

    Itens 11, 13 e 14

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,11 @@
 // implement MovieCard component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
       <main className="MovieCard">
         <section>
@@ -14,6 +15,9 @@ class MovieCard extends React.Component {
           <h4 className="movieTitle">{ title }</h4>
           <h5 className="moviesubtitle">{ subtitle }</h5>
           <p className="movieStoryline">{ storyline }</p>
+        </section>
+        <section>
+          <Rating className="rating" rating={ rating } />
         </section>
       </main>
     );
@@ -25,6 +29,7 @@ MovieCard.propTypes = {
     title: PropTypes.string,
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
+    rating: PropTypes.number,
     imagePath: PropTypes.string,
   }).isRequired,
 };
