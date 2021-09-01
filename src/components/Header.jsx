1da59a2000274dc9a3b5a0eca20588db58053d82
Riba Jr / RSTD <rstd.creative@gmail.com>
2021-08-31 21:41:30 -0300

    Requisito 1 verificado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <img></img>
+      </header>
+    )
+  }
+}
+
+export default Heeader;
 // implement Header component here
