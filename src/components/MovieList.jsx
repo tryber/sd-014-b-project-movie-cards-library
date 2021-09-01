dc5edf0146bbe7ed932b54d3124dbce924dc18ae
Ariane Ueti <arianeueti@gmail.com>
2021-09-01 17:38:53 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import data from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section className="MovieList">
+        { movies.map((obj) => <MovieCard key={ obj.title } movie={ obj } />) }
+      </section>
+    );
+  }
+}
+
+export default MovieList;
