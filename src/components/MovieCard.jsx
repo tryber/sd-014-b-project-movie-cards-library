03882cc7dafac8ffdd45781809a6be0a83abec50
Wellington_Fonseca <thyrso.kof@gmail.com>
2021-09-01 17:37:15 -0300

    Acredito que encerra o projeto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,20 @@
 // implement MovieCard component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     // Desestruturação abaixo feita dessa forma por conta do lint
     // Estava - const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
-    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+    const { movie: { title, subtitle, rating, storyline, imagePath } } = this.props;
     return (
       <section>
         <img src={ imagePath } alt={ `${title}` } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
+        <Rating rating={ rating } />
       </section>
     );
   }
@@ -22,6 +24,7 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string,
     subtitle: PropTypes.string,
+    rating: PropTypes.number,
     storyline: PropTypes.string,
     imagePath: PropTypes.string,
   }).isRequired,
