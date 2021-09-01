af4574abd7b23681eadbe00b5647209fb7f61e37
Arturo Vaine <arturo.vaine@gmail.com>
2021-09-01 15:07:04 -0300

    Requisito 1 ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    return <div><p>teste</p></div>;
+    return (
+      <header>
+        <span>Movie Cards Library</span>
+      </header>
+    );
   }
 }
 
