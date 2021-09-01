c8f4a16248f24e51700e140e80a8de3ceae06141
Jucélio Brandão <juceneon@gmail.com>
2021-09-01 15:11:39 -0300

    Impl: Style header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 // implement Header component here
 import React from 'react';
+import './Header.css';
 
 class Header extends React.Component {
   render() {
     return (
-      <header>
+      <header className="header">
         <h1>Movie Cards Library</h1>
+        <img src="https://fontmeme.com/permalink/210901/aa8947aa394b045d19d5533840b31828.png" alt="netflix-font" />
       </header>
     );
   }
