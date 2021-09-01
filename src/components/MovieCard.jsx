18e2ef9ab8786299b38c6a7b6962fc1c40deb428
Leomar Linhares <leomar_linhares@hotmail.com>
2021-09-01 16:37:47 -0300

    Cumpre o requisito 9

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,6 +10,7 @@ class MovieCard extends React.Component {
         </section>
         <section>
           <h4>{ title }</h4>
+          <h5>{ subtitle }</h5>
         </section>
       </div>
     );
