dfd6df2a3a1bf9ca3db7f1c3ea0fefbd3c4291fa
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-09-01 15:30:41 -0300

    Componente rating criado

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
     render(){
+        // const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
         const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
         return ( 
             <div>
                 <img src={imagePath} alt={`Imagem do filme ${title}`}></img>
-                <h2> {title} </h2>
-                <h3> {subtitle} </h3>
+                <h4> {title} </h4>
+                <h5> {subtitle} </h5>
                 <p> {storyline} </p>
-                <p> {rating} </p>
+                <Rating className='rating' rating={rating} />
             </div>
         )
     }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,11 @@
-// implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+    render(){
+        const rating = this.props.rating;
+        
+        return (<p> {rating} </p>)
+    }
+}
+
+export default Rating;
