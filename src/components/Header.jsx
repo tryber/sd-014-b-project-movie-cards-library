8d693ea3a03a45763b4a6eac06610aef64d5966a
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-09-01 14:36:15 -0300

    Req 1: Completo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,14 @@
 // implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Eu sou um header</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
