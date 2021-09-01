cc00479add6dfa4eedb99966755b7ec03e2b8342
Nataniel Santos <nathan.santos159@hotmail.com>
2021-09-01 19:49:57 -0300

    Requisitos 15, 16, e 17

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
-    return (movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />));
+    return (
+      <div>
+        { movies.map((film) => <MovieCard key={ film.title } movie={ film } />) }
+      </div>
+    );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  })).isRequired,
+};
 
 export default MovieList;
