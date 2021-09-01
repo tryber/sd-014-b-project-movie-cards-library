e58ddf543e2e7fe0cbfe65bfb15045c77404227b
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2021-09-01 16:07:54 -0300

    Requesito numero #1.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,18 @@
 // implement Header component here
+import React from 'react';
+import Proptypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    const { headerText } = this.props;
+    return (
+      <header>{ headerText }</header>
+    );
+  }
+}
+
+Header.propTypes = {
+  headerText: Proptypes.string.isRequired,
+};
+
+export default Header;
