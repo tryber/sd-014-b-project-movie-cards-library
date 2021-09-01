973575d38a8dbc5ba0f02b512b46cdf8e3bda260
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-09-01 01:27:49 -0300

    Requisito 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 
 class MovieCard extends Component {
   render() {
-    
+    // const { movie } = this.props;
+    return (
+      <div />
+    );
+
   }
 }
 
