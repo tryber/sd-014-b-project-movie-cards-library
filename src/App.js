e22f39cc221e98cffe117e1ab5d26f576bb10e88
Luiz Gustavo <gusstavopdm@gmail.com>
2021-09-01 15:36:06 -0300

    Requisito 3 ~~ 17

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import movies from './data';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
-    <div className="App">
+    <main className="App">
       <Header />
-    </div>
+      <MovieList movies={ movies } />
+    </main>
   );
 }
 
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,13 +1,22 @@
 // implement Rating component here
-
+import PropTypes from 'prop-types';
 import React from 'react';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      <header />
+      <div className="rating">
+        <span>
+          {rating}
+        </span>
+      </div>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+}.isRequired;
 
 export default Rating;
