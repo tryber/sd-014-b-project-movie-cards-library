ea0040cd8b7f1179a4449dbf03d4d1128692c7f2
Elton Rodrigues <el_dantas1999@hotmail.com>
2021-09-01 18:20:49 -0300

    Requisitos 6 ao 10 + Props Validation no  MovieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, imagePath, subtitle, storyline } } = this.props;
     return (
-      <section />
+      <section>
+        <h4>{title}</h4>
+        <img src={ imagePath } alt="Capa do filme {title}" />
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </section>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    imagePath: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
