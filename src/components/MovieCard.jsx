72b20a8698474f28608cb010de4c69f33f59b703
Ronaldo Junior <ronaldosenajr@gmail.com>
2021-09-01 15:19:27 -0400

    Tentando realizar o requisito 4 e 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,15 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+    return (
+      <div>
+        <img src={ imagePath } alt={ title } />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,18 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+import data from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    // const { movies } = this.props;
+    const movies = data;
+    const allMovies = movies
+      .map((item, index) => <MovieCard movie={ item } key={ index } />);
+    return (
+      { allMovies }
+    );
+  }
+}
+
+export default MovieList;
