69b4f4164f2c63775949fd9d898edb4d32ac137d
Rafael Veiga <rafaelveig@hotmail.com>
2021-09-01 19:09:50 -0300

    lint2

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import Header from './components/Header';
 import MovieList from './components/MovieList';
 import data from './data';
 
-
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movies={data} />
+      <MovieList movies={ data } />
     </div>
   );
 }
