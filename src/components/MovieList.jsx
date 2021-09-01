089e5d58286d61c0c7ed7d89e1372fe8c014e00c
Fernando Serpa <serpaef@outlook.com>
2021-09-01 15:38:02 -0300

    Implementado MovieList e criado arquivo com dados dos filmes

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
 // implement MovieList component here
+import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return movies.map((el) => <MovieCard key={ el.title } />);
+  }
+}
+
+Movies.
+
+export default MovieList;
---
+++ src/components/data.jsx
@@ -0,0 +1,63 @@
+const data = [
+  {
+    title: 'Shang-Chi and the Legend of the Ten Rings (2021)',
+    subtitle: 'Action | Adventure | Fantasy | Sci-Fi',
+    storyline: 'Shang-Chi, the master of unarmed weaponry-based Kung Fu, is'
+    + ' forced to confront his past after being drawn into the Ten Rings organization.',
+    imagePath: 'https://m.media-amazon.com/images/M/MV5BNTliYjlkNDQtMjFlNS00NjgzLWFmMWEtYmM2Mzc2Zjg3ZjEyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UY209_CR0,0,140,209_AL_.jpg',
+    rating: 71,
+  },
+  {
+    title: 'We Need to Do Something (2021)',
+    subtitle: 'Horror',
+    storyline: 'After Melissa and her family seek shelter from a storm,'
+    + ' they become trapped. With no sign of rescue, hours turn to days and'
+    + ' Melissa comes to realize that she'
+    + ' and her girlfriend Amy might have something to do with the horrors'
+    + ' that threaten to tear her family -'
+    + ' and the entire world, apart.',
+    imagePath: 'https://m.media-amazon.com/images/M/MV5BMmU0NThmNWEtMGQ5OC00YTJmLThlYzQtMWU0ODMzOWEzNmUzXkEyXkFqcGdeQXVyMTE1MzI2NzIz._V1_UY209_CR1,0,140,209_AL_.jpg',
+    rating: 62,
+  },
+  {
+    title: 'The Gateway (2021)',
+    subtitle: 'Drama | Thriller',
+    storyline: 'A social worker assigned to the care of the daughter'
+    + ' of a single mother intervenes when'
+    + ' the dad returns from prison and lures them into a life of crime.',
+    imagePath: 'https://m.media-amazon.com/images/M/MV5BY2VhMjhkODYtZWY0Yi00YjIyLThiNjAtMzRkYjZmZjNkYzgyXkEyXkFqcGdeQXVyNjIzMjk4Ng@@._V1_UX140_CR0,0,140,209_AL_.jpg',
+    rating: 65,
+  },
+  {
+    title: 'Good (2020)',
+    subtitle: 'Drama',
+    storyline: 'Payton becomes the caretaker of Gregory Devereaux, '
+    + 'a wealthy man facing his final months.'
+    + ' As they become close, Gregory\'s past sins force'
+    + ' Payton to decide between his dreams and a '
+    + 'pregnancy that could squander them all.',
+    imagePath: 'https://m.media-amazon.com/images/M/MV5BZDFiNDE1NDQtY2YxOS00OTE4LThiYjUtNWJmNDkyZTFhZTU5XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_UY209_CR3,0,140,209_AL_.jpg',
+    rating: 70,
+  },
+  {
+    title: 'Zone 414 (2021)',
+    subtitle: 'Sci-Fi | Thriller',
+    storyline: 'Set in the near future, private detective David Carmichael is'
+    + ' hired by Marlon Veidt, an eccentric businessman, to track down his'
+    + ' missing daughter. David teams up with Jane, a highly advanced A.I.'
+    + ' to solve the mystery.',
+    imagePath: '',
+    rating: 88,
+  },
+  {
+    title: 'Karen (2021)',
+    subtitle: 'Crime | Drama | Thriller',
+    storyline: 'A racist woman takes it as a personal mission to displace'
+    + ' the new Black family that just moved into the neighborhood,'
+    + ' but they won\'t back down without a fight.',
+    imagePath: 'https://m.media-amazon.com/images/M/MV5BNWZmMDY2MjEtZmE3Mi00MTg2LWJmNjktMTcyMzYxNzc1NmMzXkEyXkFqcGdeQXVyNjQ2MDAwMDM@._V1_UY209_CR8,0,140,209_AL_.jpg',
+    rating: 91,
+  },
+];
+
+export default data;
