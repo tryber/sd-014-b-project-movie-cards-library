f8cd144f21f731c21c308e8c90058206f7a47f40
Henrique Almeida <joaoh.almeidaaraujo@gmail.com>
2021-09-01 15:39:18 -0300

    Iniciando o projeto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
 // implement MovieList component here
+import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section>
+        {movies.map((info) => <MovieCard movie={ info } key={ info.title } />)}
+      </section>
+    );
+  }
+}
+
+export default MovieList;
