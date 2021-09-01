7a358ed9eeb71609114e788ac05f0e215d61ff70
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-09-01 16:10:22 -0300

    Alguns requisitos:  Necessitei fazer vários requisitos de uma vez por conta do Lint, já que  o teste não iria rodar se ele estivesse reclamando.

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import data from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <main className="movie-list">
+        { data.map((card) => <MovieCard key={ card.title } movie={ card } />) }
+      </main>
+    );
+  }
+}
+
+export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,21 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <div className="movie-card-rating">
+        <p className="rating">{ movie }</p>
+      </div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  movie: PropTypes.shape(
+    { rating: PropTypes.number },
+  ).isRequired,
+};
+
+export default Rating;
