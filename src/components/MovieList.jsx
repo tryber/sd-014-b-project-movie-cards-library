d5ebc35da9c5a4a7562e23c44eff73a055475216
Denis Espanhol <denisgfespanhol@gmail.com>
2021-09-01 17:52:26 -0300

    Cria um componente <MovieList />

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,31 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends Component {
+  render() {
+    const { movies: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    return (
+      <section>
+        <p>{title}</p>
+        <p>{subtitle}</p>
+        <p>{storyline}</p>
+        <p>{rating}</p>
+        <img src={ imagePath } alt={ title } />
+      </section>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.shape(
+    {
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    },
+  ).isRequired,
+};
+
+export default MovieList;
