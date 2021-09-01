41ef624f3fc201e2a32d67f3f5a49e9c33e6cec8
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-09-01 17:18:18 -0300

    3 e 4 done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
-// implement MovieCard component here
+import React from 'react';
+// import movies from './data';
+
+class MovieList extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie
+
+    return (
+      <section>
+        <p>
+          {title}
+          {subtitle}
+          {storyline}
+          {rating}
+        </p>
+        <img src={imagePath} alt={`magem do filme ${title}`} />
+      </section>
+    );
+  }
+}
+
+export default MovieList;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <main>
+        {movies.map((filme) => <MovieCard movie={filme} />)}
+      </main>
+    );
+  }
+}
+
+export default MovieList;
