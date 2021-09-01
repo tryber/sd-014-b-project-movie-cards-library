7778d7b0d9ecdaeaa74cf4acbd5a9c97cef81bff
icaroemanuel <icaro.emanuel@outlook.com>
2021-09-01 19:44:34 -0300

    Reuisito 5 - Renderizando o subtitulo

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,14 +4,15 @@ import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { 
-      movie: { title, subtitle, storyline, imagePath, rating } 
+    const {
+      movie: { title, subtitle, storyline, imagePath, rating },
     } = this.props;
 
     return (
       <section>
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
       </section>
     );
   }
