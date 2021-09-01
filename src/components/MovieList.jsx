6b5f759d3aae3041420326b61ff7ea4ebf5fdddc
José Victor Macedo Veloso <vitovls37@gmail.com>
2021-09-01 15:12:29 -0300

    Requisito 3 - Criar component `<MovieList />`

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return <section />;
+  }
+}
+
+export default MovieList
