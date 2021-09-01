8e22031983ef4551b6749e9df918f5b40240231d
enzo <enzo@pop-os.localdomain>
2021-09-01 15:00:55 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
 // implement MovieList component here
+
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath} = this.props.movies
+    return (
+      <div>
+        <h1>{ title }</h1>
+        <h1>{ subtitle }</h1>
+        <h1>{ storyline }</h1>
+        <h1>{ rating }</h1>
+        <h1>{ imagePath }</h1>
+      </div>
+    );
+  }
+}
+
+export default MovieList;
