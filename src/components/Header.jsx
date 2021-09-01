6061db0ef55716d768045886024412d97f40fba2
Luana <luana.san2015.ls@gmail.com>
2021-09-01 15:11:53 -0300

    Requisito 1 - Crie um componente <Header />.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-// implement Header component her
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <h1> </h1>
+    );
+  }
+}
+
+export default Header;
