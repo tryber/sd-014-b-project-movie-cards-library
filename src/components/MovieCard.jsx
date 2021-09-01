ce7235b10cb82f82f4aa0ecf9c5a3881457e669e
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-09-01 15:58:42 -0300

    test lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,24 @@
 // implement MovieCard component here
 import React, { Component } from 'react';
+// import PropType from 'prop-types';
 
 class MovieCard extends Component {
   render() {
+    // const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div />
     );
   }
 }
+
+// MovieCard.propTypes = {
+//   movie: PropTypes.shape({
+//     title: PropTypes.string,
+//     subtitle: PropTypes.string,
+//     storyline: PropTypes.string,
+//     rating: PropTypes.number,
+//     imagePath: PropTypes.string,
+//   }),
+// };
 
 export default MovieCard;
