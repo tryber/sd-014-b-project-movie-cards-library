8b2dbae19c5d5c862a814e89c506920cbb971c46
Elithon Silva <elithonsilva@gmail.com>
2021-09-01 16:23:16 -0300

    impl: cria componente MovieCard, renderiza img filme, titulo e subtitulo

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+    return (
+      <div>
+        <img src={ imagePath } alt={ `imagem do filme ${title}` } />
+        <h4>{title}</h4>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      movies.map((theMovie) => <MovieCard movie={ theMovie } key={ theMovie.title } />)
+    );
+  }
+}
+
+export default MovieList;
