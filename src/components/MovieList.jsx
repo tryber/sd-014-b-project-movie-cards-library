c54b446ef5571bda475c60d5976d93ec335618ce
eliHC <leonardohcds@gmail.com>
2021-09-01 15:38:02 -0300

    fix lint?

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+// const arrayofMovies = ['x', 'c', 'v', 'x']; // ????
+
+// movies.map((movie) => <MovieCard />)
 
 class MovieList extends React.Component {
   render() {
-  const { movies } = this.props;
+  const { movies } = this.props.movies;
   
     return (
-      <main>teste de retorno
-        <ul>
-          <li>1</li>
-          <li>2</li>
-          <li>3</li>
-        </ul>
+      <main>
+        <MovieCard />  
+        <MovieCard />
+        <MovieCard />
       </main>
     );
   };
