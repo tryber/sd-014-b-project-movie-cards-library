9f34a56300c48b4844b555a9d14a390d09a66a1b
Levi Manoel <levimanoel.deob@gmail.br>
2021-09-01 15:04:25 -0300

    Requisitos 6, 7, 8, 9, 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
-      <div />
+      <div>
+        <img src={imagePath}/>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </div>
     );
   }
 }
