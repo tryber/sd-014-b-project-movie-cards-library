a2580f6f4817729a6ab21524f7c8f96011f895db
Arturo Vaine <arturo.vaine@gmail.com>
2021-09-01 15:09:57 -0300

    Requisito 2 ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <span>Movie Cards Library</span>
+        <h1>Movie Cards Library</h1>
       </header>
     );
   }
