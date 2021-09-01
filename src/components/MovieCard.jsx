58a8d5e9ff180a25068c63e8181407ebef734057
Danilo <danilo.santossouza@outlook.com>
2021-09-01 15:51:04 -0300

    Resolvendo problema lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
-      <div>
+      <section>
         <img src={ imagePath } alt={ title } />
-        <h4>{ title }</h4>
+        <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-      </div>
+        <Rating />
+      </section>
     );
   }
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,13 @@
-// implement MovieList component here
 import React from 'react';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const movies = this.props;
+    const { movie } = this.props;
     return (
-      <div>
-        MovieList
-      </div>
+      <main>
+        {movie.map((movieData) => <MovieCard key={ movieData.title } movie={ movieData } />) }
+      </main>
     );
   }
 }
