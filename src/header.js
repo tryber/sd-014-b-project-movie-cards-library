c721f8845a7912161e4ef0f4b14088884265f3cd
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-09-01 15:10:01 -0300

    [Rafael Moraes] Projeto Movie Cards Library: Requisito 1 e 2

--- src/header.js
+++ src/header.js
@@ -4,7 +4,7 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <h1>Um Parágrafo no Header</h1>
+        <h1>Movie Cards Library</h1>
       </header>
     );
   }
---
+++ src/movielist.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <main>
+        <h1>Movie List Section</h1>
+      </main>
+    );
+  }
+}
+
+export default MovieList;
