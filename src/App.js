ac85acfb45b77bd8d95f6285655f2c53880fef9a
Elithon Silva <elithonsilva@gmail.com>
2021-09-01 16:57:40 -0300

    renderiza <MovieList /> recebendo data.js como props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Data from './data';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ Data } />
       Hora de codar! 🚀✍️👨‍💻👩‍💻💪
     </div>
   );
