0d4402de2ef812d71e11a462296ae5f5929e3dac
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-09-01 14:37:36 -0300

    Req 2: Completo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ class Header extends React.Component {
   render() {
     return (
       <header>
-        <h1>Eu sou um header</h1>
+        <h1>Movie Cards Library</h1>
       </header>
     );
   }
