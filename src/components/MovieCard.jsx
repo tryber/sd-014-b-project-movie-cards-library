a77336b29fee25ac6acc6ad0d3bbe2f9a321dc53
Leomar Linhares <leomar_linhares@hotmail.com>
2021-09-01 16:35:19 -0300

    Cumpre o requisito 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,12 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
     return (
       <div key={ title }>
-        
+        <section>
+          <img src={ imagePath } alt={`Imagem do filme ${title}`} />
+        </section>
+        <section>
+          
+        </section>
       </div>
     );
   }
