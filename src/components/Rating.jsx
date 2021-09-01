73bdf8c827f59db644626ce9115e32e342ed8e44
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-09-01 18:27:26 -0400

    Requisito 11

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,12 @@
-// implement Rating component here
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    return (
+      <div> rating </div>
+    );
+  }
+}
+
+export default Rating;
