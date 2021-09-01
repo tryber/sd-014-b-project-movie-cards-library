1e78ad1e599cf6de7b00f1f9ab883ba91cf28e33
vanim <vanimouzad123@gmail.com>
2021-09-01 14:47:56 -0300

    Requisito 3 - Componente MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,12 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    // const movies = this.props;
+
+    return '';
+  }
+}
+
+export default MovieList;
