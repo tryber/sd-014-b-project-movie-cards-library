cfc71e40aa57b71952fbf80581c4244881b793fa
Lucas Alves <lucalves.rodrigues@gmail.com>
2021-09-01 14:39:03 -0300

    Requisito 1 e 15

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,12 @@
 // implement Header component her
+import React from 'react';
+
+class Header extends React.Component {
+    render () {
+        return (
+            <header>Aqui é o header</header>
+        )
+    }
+}
+
+export default Header;
