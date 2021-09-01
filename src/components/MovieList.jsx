8a523e445223e4694971fbe8eee5a2caa27fba91
Davisralves <davialves324@gmail.com>
2021-09-01 16:33:16 -0300

    finalizando requisito 6

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,7 +4,7 @@ import MovieCard from './MovieCard';
 class MovieList extends React.Component {
   render() {
     const movies = this.props.movies;
-    return ( movies.map( (movie) => <MovieCard movies = {movie} key = {movie.title}/> ) );
+    return ( movies.map( (movie) => <MovieCard movie = {movie} key = {movie.title}/> ) );
   }
 }
 
