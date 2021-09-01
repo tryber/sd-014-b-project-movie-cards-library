bac187a6fdfdf1cba1b5c416ed3270e608dfe2df
Juliana Braga <juliana4braga@gmail.com>
2021-09-01 16:04:03 -0300

    Constroi MovieCard

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import Data from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      Data.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+    );
+  }
+}
+
+export default MovieList;
