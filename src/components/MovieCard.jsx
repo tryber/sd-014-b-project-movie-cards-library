2926d432825f8eb4349fd204bed6e9ae84a98a37
Davisralves <davialves324@gmail.com>
2021-09-01 16:38:47 -0300

    Finalizando requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,8 +5,10 @@ class MovieCard extends React.Component {
     const {title, subtitle, storyline, rating, imagePath,} = this.props.movie;
     return (
       <section>
-        <h4>{title}</h4>
-        <img src = {imagePath}/>
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <img src = { imagePath }/>
+        <p>{ storyline }</p>
       </section>
       );
   }
