5aad1fc628fb8abd4db4918ec6c9fbf50f9d7e29
Fernando Serpa <serpaef@outlook.com>
2021-09-01 14:38:34 -0300

    implementado componente <Header />

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Iniciando o projeto
+      <Header />
     </div>
   );
 }
