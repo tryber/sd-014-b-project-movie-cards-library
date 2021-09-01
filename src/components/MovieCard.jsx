4b00d8fd837012f6b223a16c1e04cef04e7b8252
Riba Jr / RSTD <rstd.creative@gmail.com>
2021-09-01 14:31:45 -0300

    Question 1 and 2 resolved, and lint corrections

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import movies from '../data'
+
+console.log(movies[0].storyline);
+class MovieCard extends React.Component {
+  render() {
+    return (
+      //
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class MovieList extends Component {
+  render() {
+    return (
+      <div>
+        //
+      </div>
+    )
+  }
+}
+
+export default MovieList;
