c18a917608c710f3c352ffb1249e0a4beb0de469
Patrick Paredes <patrickparedes44@gmail.com>
2021-09-01 18:28:08 -0400

    components MovieList

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import data from './data';
 
 function App() {
   return (
     <section className="App">
       <Header />
+      <MovieList movies={ data } />
     </section>
   );
 }
