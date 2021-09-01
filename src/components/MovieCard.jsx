e11c1fc677e21ae5a65d6f220bc0dd35a141d240
Rafael Moraes - Turma 14 - Tribo B <rafael1moraes1rocha@gmail.com>
2021-09-01 15:55:00 -0300

    [Rafael Moraes] Projeto Movie Cards Library: Requisito 1 e 2

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,27 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props;
+    return (
+      <div>
+        <h1>{ title }</h1>
+        <p>{ subtitle }</p>
+        <p>{ storyline }</p>
+        <p>{ rating }</p>
+        <img src={ imagePath } alt={ `Cartaz de ${title}` } />
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  imagePath: PropTypes.string.isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
 // implement MovieList component here
+import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <main>
+        <h1>Movie List Section</h1>
+        <div>
+          { movies.map((movie) => <MovieCard movieObject='movie'/>)}
+        </div>
+      </main>
+    );
+  }
+}
+
+export default MovieList;
