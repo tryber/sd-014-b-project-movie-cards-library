fbc006aeb06e4845c69c0fb799465b7f61bbbed0
Leomar Linhares <leomar_linhares@hotmail.com>
2021-09-01 15:48:07 -0300

    Cria parte do MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,27 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    const data = this.props.data;
+    return (
+      <main>
+        {`${data[1].title}`}
+      </main>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf({
+    movie: PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  }),
+};
+
+export default MovieList;
