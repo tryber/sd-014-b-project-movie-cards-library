a9a97214b42ff97561b48449321a9544a618de28
Levi Manoel <levimanoel.deob@gmail.br>
2021-09-01 15:39:24 -0300

    MovieList: lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    const movies = this.props.movies;
+    const { movies } = this.props;
     return (
       <main>
-        {movies.map((movie) => <MovieCard key={movie.title} movie={movie}/>)}
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
       </main>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })).isRequired,
+};
 
 export default MovieList;
