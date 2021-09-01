587fbda736d6de3bd455c5dfb4a3623a346dd0e8
andremorsch <andremorsch37@gmail.com>
2021-09-01 15:55:15 -0300

    requisito 1 e 2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,11 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const { title, subtitle, storyLine, rating, imagePath} = this.props.movies
+    return (
+      <div></div>
+    )
+  }
+}
