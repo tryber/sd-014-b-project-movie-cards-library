c605bce5f0da846346e82ffde697e48c833e9a0c
Mateus Gambaro <magambaro@hotmail.com>
2021-09-01 18:09:36 -0300

    Commitao bomba! Cria o componente Rating adicionando a estrutura do movieCard que em seguida tem o componente MovieList  adicionada ao app

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,17 +1,35 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
 
-    return(
+    return (
       <main>
-        { movies.map((movie) => <MovieCard/>)}
+        { movies.map((movie) => (
+          <MovieCard
+            key={ movie.title }
+            movie={ movie }
+          />
+        )) }
       </main>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default MovieList;
