a224cab38f8e1b838905a007f5d42168436fb86e
Elton Rodrigues <el_dantas1999@hotmail.com>
2021-09-01 19:15:41 -0300

    Requisito 11 + PropType Validation no Rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      <section />
+      <section>{rating}</section>
     );
   }
 }
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 export default Rating;
