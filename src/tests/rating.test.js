ef404396f8860fa6865ac318e092d8605cc8a29a
José Victor Macedo Veloso <vitovls37@gmail.com>
2021-09-01 18:04:34 -0300

    Requisito 11 - Criar um componente `<Rating />`

--- src/tests/rating.test.js
+++ src/tests/rating.test.js
@@ -30,7 +30,7 @@ const movies = [
 ];
 
 describe('11 - Crie um componente `<Rating />`', () => {
-  it.skip('Renderize o componente `<Rating />`', () => {
+  it.only('Renderize o componente `<Rating />`', () => {
     shallow(<Rating />);
   });
 });
