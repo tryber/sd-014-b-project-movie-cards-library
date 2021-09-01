b0751c5f61dbb2c54d43231fe82f5637df6d6f4f
Thiago Frozzi <thiagofrozzi@hotmail.com>
2021-09-01 15:19:35 -0300

    Movie card feito

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 class MovieCard extends React.Component {
     render(){
-
-        return ( )
+        const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+        return ( 
+            <div>
+                <img src={imagePath} alt={`Imagem do filme ${title}`}></img>
+                <h2> {title} </h2>
+                <h3> {subtitle} </h3>
+                <p> {storyline} </p>
+                <p> {rating} </p>
+            </div>
+        )
     }
 }
+
+export default MovieCard;
