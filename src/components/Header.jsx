5b612fe17d92a3c956f0736031bd096f103c3189
Cassio Mascarenhas <csmlvx@gmail.com>
2021-09-01 15:07:12 -0300

    Corrigindo ESLINT

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 class Header extends React.Component {
-    render() {
-        return <h1>Olá mundo!</h1>
-    }
+  render() {
+    return <h1>Olá mundo!</h1>;
+  }
 }
 
 export default Header;
