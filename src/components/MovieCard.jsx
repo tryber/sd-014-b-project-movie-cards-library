54ce5083f55441d0b6eda687826727f6861c99f8
eliHC <leonardohcds@gmail.com>
2021-09-01 15:03:40 -0300

    1 to 3, 15 and 16

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,16 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    return(
+      <p>moviecard component</p>
+    );
+  ;}
+};
+
+MovieCard.propTypes = {
+
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,24 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+  const { movies } = this.props;
+  
+    return (
+      <main>teste de retorno
+        <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+      </main>
+    );
+  };
+};
+
+MovieList.propTypes = {
+
+};
+
+export default MovieList;
