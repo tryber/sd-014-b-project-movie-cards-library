ccf47c4bc27f0fdafd89e94659a517fc6668683d
Cecilia Cacais <ceci.cacais@gmail.com>
2021-09-01 16:02:10 -0300

    Texto do Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,18 @@
-// import React from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// class Header extends React.Component {
-//   render() {
-//     return <header></header>
-//   }
-// }
+class Header extends React.Component {
+  render() {
+    const { frase } = this.props;
+    return (
+      <header>
+        <p>{ frase }</p>
+      </header>
+    );
+  }
+}
+Header.propTypes = {
+  frase: PropTypes.string.isRequired,
+};
+
+export default Header;
