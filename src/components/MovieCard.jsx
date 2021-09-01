dd0d5af6ba4ff5fb06e771a6def687d7ce29d3c2
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-09-01 16:24:56 -0300

    Lint e Eslint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,10 +8,14 @@ class MovieCard extends React.Component {
     return (
       <section className="movie-card">
         <div className="movie-card-body">
-          <img className="movie-card-image" src={ imagePath } alt={ `Imagem do jogo ${subtitle}` } />
+          <img
+            className="movie-card-image"
+            src={ imagePath }
+            alt={ `Imagem do jogo ${subtitle}` }
+          />
           <h4 className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
-          <p className="movie-card-storyline">{storyline}</p>
+          <p className="movie-card-storyline">{ storyline }</p>
           <Rating movie={ rating } />
         </div>
       </section>
