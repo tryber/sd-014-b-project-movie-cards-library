12c53a4a21d37a0fb3b69d3961981ed41720b910
Victoria Lorena <victorialorenaf.gomes@gmail.com>
2021-09-01 18:10:55 -0300

    Itens 4, 5 e 6

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,21 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <section />
+      <section className="MovieList">
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </section>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
