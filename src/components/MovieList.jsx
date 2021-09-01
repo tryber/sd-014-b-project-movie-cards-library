93525011949d7c87e441711c27f1fb9f99f1d9c9
Daniel <daniel.acoelho@outlook.com>
2021-09-01 17:11:51 -0300

    Commit com correção de Lint e alguns requisitos

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+    render() {
+        const { title, subtitle, storyline, rating, imagePath } = this.props.movies 
+        return (
+          <section>
+            <p><strong>{ title }</strong>,
+              { subtitle },
+              { storyline },
+              { rating }
+            </p>
+            <img src={imagePath}  alt={'Imagem do filme ${title}'} />
+          </section>
+        )
+    }
+}
+
+export default MovieList;
