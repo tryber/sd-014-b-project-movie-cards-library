fd22c444bfaadccbbc4b712eaca750be6c75a3bd
Ariane Ueti <arianeueti@gmail.com>
2021-09-01 19:06:15 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import MovieCard from './MovieCard';
-import data from '../data';
+
 
 class MovieList extends React.Component {
   render() {
-    const { movies } = this.props;
     return (
-      <section className="MovieList">
-        { movies.map((obj) => <MovieCard key={ obj.title } movie={ obj } />) }
-      </section>
+      <p> Teste </p>
     );
   }
 }
