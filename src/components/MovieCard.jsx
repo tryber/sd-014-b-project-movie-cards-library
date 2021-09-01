82ace4407f99f967aef19de8195fe6f7084f1a5e
Wallacy Francis <wallacy_francis@hotmail.com>
2021-09-01 19:35:33 -0300

    Resolvido um tanto de requisito mas não sei onde estou =)

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,36 @@
-// implement MovieCard component here
+import React from 'react';
+import PropType from 'prop-types';
+
+class MovieCards extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    return (
+      <section>
+        <h4>{ title }</h4>
+        <img src={ imagePath } alt={ title } />
+        <h5>
+          {subtitle}
+        </h5>
+        <p>
+          {storyline}
+        </p>
+        <p>
+          {rating}
+        </p>
+
+      </section>
+    );
+  }
+}
+
+MovieCards.propType = {
+  movie: PropType.shape({
+    title: PropType.string,
+    subtitle: PropType.string,
+    storyline: PropType.string,
+    rating: PropType.number,
+    imagePath: PropType.string,
+  }).isRequired,
+};
+
+export default MovieCards;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+import '../App.css';
+import MovieCards from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    const MovieLst = movies.map((movie, index) => (<MovieCards key={ index } movie={ movie } />));
+    return (
+      <section>
+        { MovieLst }
+      </section>
+    );
+  }
+}
+
+export default MovieList;
