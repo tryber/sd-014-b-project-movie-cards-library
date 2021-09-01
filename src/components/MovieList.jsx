89bacd81aeac523b98bc04e833fea289fed54b09
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-09-01 16:57:22 -0300

    Requisito 4

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
 
-    return
+    return (
+      <main>
+        {movies.map((movie) => <MovieCard movie={ movie } />)}
+      </main>
+    );
   }
 }
 
