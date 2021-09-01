06d7dd4f2ed11e1a78c431cd0645feff170d1a45
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-09-01 14:51:14 -0300

    Req 3: Completo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import MovieList from './components/MovieList';
 import Header from './components/Header';
 import './App.css';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList />
     </div>
   );
 }
