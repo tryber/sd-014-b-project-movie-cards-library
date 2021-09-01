5c20f45c8f377668bd18d71bf939dcc4ce690601
perchespierri <perchespierri@gmail.com>
2021-09-01 20:56:29 -0300

    CHEGA. TOTAL

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
-import movies from '../data';
+// import movies from '../data';
 // Por que não precisa de .js depois de data?
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
-export default class MovieList extends Component {
+class MovieList extends Component {
   render() {
     const { movies } = this.props;
     return (
-      <div className="movielist">
+      <div>
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }
 }
+
+// MovieList.propTypes = {
+//   movies: PropTypes.shape({
+//     title: PropTypes.string,
+//     subtitle: PropTypes.string,
+//     storyline: PropTypes.string,
+//     imagePath: PropTypes.string,
+//     rating: PropTypes.number,
+//   }).isRequired,
+// };
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+export default MovieList;
