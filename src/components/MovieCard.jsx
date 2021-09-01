5a96dbeb2052e05c8ac3fb6cdf746adb0fa92ff2
Fabrício Cardoso <fabricio_csi52@hotmail.com>
2021-09-01 01:37:33 -0300

    Requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,6 +9,7 @@ class MovieCard extends Component {
         <img src={ imagePath } alt={ `Imagem do filme ${title}` } />
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p>
       </div>
     );
   }
