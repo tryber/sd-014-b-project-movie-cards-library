f87102d3f9b19dd0fed874225973733bdd6c439b
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-09-01 18:56:04 -0300

    estruturando o movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import movies from '../data';
 
 class MovieCard extends React.Component {
   render() {
-    return <div>movie movie</div>;
+    const {movie:{title, subtitle, storyline, imagePath, rating}} = this.props;
+    return (
+      <div>
+        <h2>{title}</h2>
+        <h3>{subtitle}</h3>
+        <p>{storyline}</p>
+        <img src={ imagePath } alt="capa do filme" />
+        <p>{rating}</p>
+      </div>);
   }
 }
 
 MovieCard.propType = {
-  text: PropTypes.string.isRequired,
+  movies: PropTypes.arrayOf,
+};
+MovieCard.defaultProps = {
+  movies: 'Faltou filmes',
 };
 
 export default MovieCard;
