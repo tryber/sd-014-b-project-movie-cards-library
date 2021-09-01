497d2f12eeb10799e5f33b6f57151ac3df287a40
Thyara <thyaranonato@outlook.com>
2021-09-01 18:55:42 -0300

    Adiciona estilo aos arquivos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,13 +6,15 @@ class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
-      <section>
-        <img src={ imagePath } alt={ `Imagem do filme ${title}` } />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
-        <Rating rating={ rating } />
-      </section>
+      <div className="movie-card-body">
+        <section className="movie-card">
+          <img className="movie-card-image" src={ imagePath } alt="" />
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+          <Rating rating={ rating } />
+        </section>
+      </div>
     );
   }
 }
