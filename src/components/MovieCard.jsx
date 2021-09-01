8d566168ada1167e3d3f1e37c0159f427be28a8d
Arturo Vaine <arturo.vaine@gmail.com>
2021-09-01 16:36:34 -0300

    Requisito 16 ok

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,33 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movies: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    return (
+      <div className="pokemon">
+        <div>
+          <p>{ title }</p>
+          <p>{ subtitle }</p>
+          <p>
+            {`Sinopsis: ${storyline}`}
+          </p>
+          <p>{ rating }</p>
+        </div>
+        <img src={ imagePath } alt={ `Poster from the movie ${title}` } />
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movies: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     return (
       <div>
         <p>Movie list</p>
+        <div className="MovieCard">
+          {movies.map((movies) => <MovieCard key={ movies.id } movies={ movies } />)}
+        </div>
       </div>
     );
   }
