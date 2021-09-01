796d5f9ff77bd6d53677e6ce609350059d2eda86
= <luz.matheus11@gmail.com>
2021-09-01 14:47:28 -0300

    Requisito 1 e 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,16 @@
+// const react = require("react");
+
 // implement Header component here
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
