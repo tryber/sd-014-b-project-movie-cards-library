fbf60d36471b7033470708c8a605820c88d95a9d
vanim <vanimouzad123@gmail.com>
2021-09-01 16:51:47 -0300

    Adiciona PropTypes

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,24 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
+
     return movies.map((movie) => <MovieCard movie={ movie } key={ movie.title } />);
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  })).isRequired,
+};
 
 export default MovieList;
