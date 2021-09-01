e961f82eaf1048346ac4098f74429cb5eb5c53b1
Mateus Gambaro <magambaro@hotmail.com>
2021-09-01 17:16:41 -0300

    Adiciona Estrutura para componente MovieList e MovieCard

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return(
+      <main>
+        { movies.map((movie) => <MovieCard/>)}
+      </main>
+    );
+  }
+}
+
+export default MovieList;
