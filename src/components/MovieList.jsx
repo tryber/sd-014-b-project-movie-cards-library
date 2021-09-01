206d601436bbd2de13b89c9784523406b0e2311c
Nataniel Santos <nathan.santos159@hotmail.com>
2021-09-01 17:34:45 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,9 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const {movies} = this.props;
+  }
+}
+
+export default MovieList;
