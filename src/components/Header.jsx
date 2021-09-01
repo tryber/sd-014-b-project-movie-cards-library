856718fd4bc2cf8b2a14f0165231f5f42332fbaf
LuanaMB <luanabragaaa@hotmail.com>
2021-09-01 15:45:50 -0300

    até o 10

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <header className="movie-card-header">
+          <h1>Movie Cards Library</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Header;
