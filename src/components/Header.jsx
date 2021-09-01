68a6149b77e1a5358e6f22a16a7ceaf06984fc31
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-09-01 15:54:04 -0300

    Iniciando o projeto rodando os testes e começando a criar os componetes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,19 @@
-// implement Header component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    const title = 'Movie Cards Library';
+    return (
+      <header>
+        <h1>{title}</h1>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
