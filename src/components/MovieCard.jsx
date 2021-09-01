30d1916e87e8026b60214cba8629431265bb5ebb
Juliana Braga <juliana4braga@gmail.com>
2021-09-01 18:23:25 -0300

    Aplica estilo css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,12 +6,16 @@ class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
-      <section>
-        <img src={ imagePath } alt={ `Imagem do filme ${title}` } />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
-        <Rating rating={ rating } />
+      <section className="movie-card-header movie-card">
+        <img
+          className="movie-card-image"
+          src={ imagePath }
+          alt={ `Imagem do filme ${title}` }
+        />
+        <h4 className="movie-card-title">{ title }</h4>
+        <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p>
+        <Rating rating={ rating } className="movie-card-rating" />
       </section>
     );
   }
