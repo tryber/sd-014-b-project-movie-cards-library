074cd87054cd45e711757dad02ad396a89532354
Humberto B. Bonadiman <humberto_bonadiman@hotmail.com>
2021-09-01 15:53:48 -0400

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
+import Header from './components/Header';
+// import movies from './data';
 
 function App() {
   return (
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section></section>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.string.isRequired,
+};
+
+export default MovieList;
