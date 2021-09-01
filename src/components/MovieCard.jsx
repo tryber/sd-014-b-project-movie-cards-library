621d9465a1b4a6da03c23f9aa1e2c9e46b376cee
Victoria Lorena <victorialorenaf.gomes@gmail.com>
2021-09-01 18:39:01 -0300

    Item 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,25 @@
 // implement MovieCard component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, imagePath } } = this.props;
     return (
-      <section />
+      <main className="MovieCard">
+        <section>
+          <img className="ImagesCard" src={ imagePath } alt={ title } />
+        </section>
+      </main>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
