2004f22a306162a5533748e03aa45c77fc89b076
killu4kun <mateeus.soarees@outlook.com>
2021-09-01 16:12:07 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,10 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+
+export default class MovieList extends Component {
+  render() {
+    return (
+      <div />
+    );
+  }
+}
