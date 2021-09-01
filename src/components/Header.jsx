9e70f8308dc67d86befef503bbdc17cf2a08c1e1
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-09-01 15:48:57 -0300

    2.done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
+    const texto = 'Movie Cards Library';
+
     return (
       <header>
-        cabeçalho
+        <h1>
+          {texto}
+        </h1>
       </header>
     );
   }
