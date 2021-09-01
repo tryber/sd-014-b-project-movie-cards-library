84d8e1d86f02eda5d022494bbfb849635c07ece5
anastacioneto <anastacio.a.c.n@gmail.com>
2021-09-01 16:17:36 -0300

    impl: requirement 1 and 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
 
+const text = 'Movie Cards Library';
+class Header extends React.Component {
+	render () {
+		return (
+			<header>
+				<h1>
+					{ text }
+				</h1>
+			</header>
+		)
+	}
+}
+
+export default Header;
