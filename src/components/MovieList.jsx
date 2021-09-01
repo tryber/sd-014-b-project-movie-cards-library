eb64a3b070f66185c57d8460e3566a36566f4163
Bruno M Landim <brunolandim13@hotmail.com>
2021-09-01 16:50:56 -0300

    Ajuste do erro de lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    return (
+      <section>
+        <img src={ imagePath } alt={ `Imagem do filme ${title}` } />
+        <h2>{ title }</h2>
+        <h3>{ subtitle }</h3>
+        <p>{ storyline }</p>
+        <p>{ rating }</p>
+      </section>
+    );
+  }
+}
+
+export default MovieList;
