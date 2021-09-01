a6c2744fc619d878ff61af0358a416b4deb575cf
Gustavo <gus.inacio@hotmail.com>
2021-09-01 15:23:27 -0300

    Requirements #1 & #2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,19 @@
-// implement Header component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    const { text } = this.props;
+    return (
+      <header>
+        <h1>{text}</h1>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+export default Header;
