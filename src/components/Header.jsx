8ff063ee36c6bc71057ba538e0c7359419200300
natalia <mellomartins.naty@gmail.com>
2021-09-01 15:28:46 -0300

    requisitos 1 e 2

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render(){
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    )
+  }
+}
+
+export default Header;
+// Header.PropTypes = {  
+// }
