73e65a81a57fbcc394f2547bff137c85c166800c
Paulo França <paulovinicius.franca@gmail.com>
2021-09-02 16:24:51 -0300

    Início da resolução do MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
-    return {};
+    const { movieList } = this.props;
+    return (
+      <section>
+        { movieList.map((filme, index) => <MovieCard key={ index } movie={ filme } />) }
+      </section>
+    );
   }
 }
+
+// MovieList.propTypes = {
+//   movies: PropTypes.shape,
+// };
 
 export default MovieList;
