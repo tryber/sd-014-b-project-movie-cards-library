6d1426a67a9fcf563aea7ef3bb993e8882fdd614
mfelipperd <felipperabelodurans@gmail.com>
2021-09-01 23:24:27 -0300

    rquisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import movies from '../data';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    // const { movie } = this.props;
+    const { movie } = this.props;
     return (
-      <div>
-        <img src={ movies.imagePath } alt="imagem do filme" />
-        <h4 className="movie-title">{ movies.title }</h4>
+      <div calssName="movie-card">
+        <div className="movie-body">
+          <img
+            src={ movie.imagePath }
+            alt="imagem do filme"
+            className="movie-card-imagem"
+          />
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p calssName="movie-card-storyline">{ movie.storyline }</p>
+        </div>
       </div>
     );
   }
 }
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
 
 export default MovieCard;
