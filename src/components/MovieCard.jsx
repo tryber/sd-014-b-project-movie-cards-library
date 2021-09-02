9f27ebae19bfb3e3ed2913826e0124c132dc8f3b
igor.marinho <igormarinhoargollo@gmail.com>
2021-09-01 22:08:07 -0300

    Movie Cards Library - Requeriment 8

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,10 @@ class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
-      <img src={ imagePath } alt={ `ìmagem do filme ${title}` } />
+      <section>
+        <img src={ imagePath } alt={ `ìmagem do filme ${title}` } />
+        <h4>{ title }</h4>
+      </section>
     );
   }
 }
