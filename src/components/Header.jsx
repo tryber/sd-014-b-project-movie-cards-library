f715a36ba29dc205ab9a210f7ad89e9a7f49c236
anastacioneto <anastacio.a.c.n@gmail.com>
2021-09-02 11:40:21 -0300

    adding rendering

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+// import PropTypes from 'prop-types';
 
-const text = 'Movie Cards Library';
 class Header extends React.Component {
   render() {
     return (
       <header className="movie-card-header">
-        <h1 className="page-title">
-          { text }
-        </h1>
+        <h1 className="page-title">Movie Cards Library</h1>
       </header>
     );
   }
 }
+
+// Header.propTypes = {
+//   text: PropTypes.shape({
+//     text: PropTypes.string,
+//   }).isRequired,
+// };
 
 export default Header;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-// import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
