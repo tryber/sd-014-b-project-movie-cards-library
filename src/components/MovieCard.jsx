c856947cfc2d6ac446304f33ca4005ea06752183
lucas <mr.pine97@gmail.com>
2021-09-02 19:06:12 -0300

    impl: Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 export default class MovieCard extends Component {
   render() {
@@ -10,7 +11,7 @@ export default class MovieCard extends Component {
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
-        <h2>{ rating }</h2>
+        <Rating rating={ rating } />
       </section>
     );
   }
