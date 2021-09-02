e7fecea28406566ab1621a86a287679c86a51dc1
mfelipperd <felipperabelodurans@gmail.com>
2021-09-01 23:07:09 -0300

    imagem e titulo

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import movies from '../data';
 // import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     // const { movie } = this.props;
     return (
-      <div> oi </div>
+      <div>
+        <img src={ movies.imagePath } alt="imagem do filme" />
+        <h4 className="movie-title">{ movies.title }</h4>
+      </div>
     );
   }
 }
