b976ec15d03e4420168e9b98942298dffe8ed22d
Paulo França <paulovinicius.franca@gmail.com>
2021-09-02 14:15:52 -0300

    Requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyLine, imagePath, rating } = this.props.movie;
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div>
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <img src={ imagePath } alt={ `Imagem do filme ${title}` } />
-        <p>{storyLine}</p>
-        <p>{rating}</p>
+        <p>{storyline}</p>
       </div>
     );
   }
