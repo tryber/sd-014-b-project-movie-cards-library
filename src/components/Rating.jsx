656b2baba16cd0b26b45f57724d46c4aee6fb2d7
brenoMatias <matiasbreno19@gmail.com>
2021-09-02 10:53:18 -0300

    rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,16 @@
 // implement Rating component here
+import React from 'react'; // JSX transform turns it into regular JavaScript:
+
+class Rating extends React.Component { // component estilo class
+    render() { 
+    const rating = this.props.rating;
+        return ( 
+            <div className="rating">
+                <p> Rating {rating} </p>
+               
+            </div>
+        );
+    }
+}
+
+export default Rating;
