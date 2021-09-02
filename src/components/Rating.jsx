c8c6f67c3a4a4375cf7cc597ec943aad7a485538
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-09-01 23:11:16 -0300

    final lint

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,10 +2,9 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
-
 class Rating extends Component {
   render() {
-    const { rating } = this.props
+    const { rating } = this.props;
     return (
       <div>
         <p className="rating">{rating}</p>
