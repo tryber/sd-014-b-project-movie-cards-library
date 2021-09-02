b8d306b617ab6090f1171825f05d7ef7cf5578aa
igor.marinho <igormarinhoargollo@gmail.com>
2021-09-01 21:54:07 -0300

    PropTypes adjustment

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,20 +3,19 @@ import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
-    const { movie } = this.props;
-    return `${movie.title} `;
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    return 'oi';
   }
 }
 
 MovieCard.propTypes = {
-  movie: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string,
-      subtitle: PropTypes.string,
-      storyline: PropTypes.string,
-      rating: PropTypes.number,
-      imagePath: PropTypes.string,
-    }),
-  ).isRequired,
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
 };
+
 export default MovieCard;
