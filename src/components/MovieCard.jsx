69a760ba1f7f27e2d9c540275aa6694a3260968a
FelipeMartins <mfelipeluiz99@gmail.com>
2021-09-01 23:04:00 -0300

    requisitos 4, 5, 6 e 7 and LINT FIX

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,28 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends Component {
+  render() {
+    const {
+      movie: { title, subtitle, storyLine, imagePath, rating },
+    } = this.props;
+    return (
+      <div>
+        <img src={ imagePath } alt="imagem" />
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,31 @@
 // implement MovieList component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
-export default class MovieList extends Component {
+class MovieList extends Component {
   render() {
-    return <div>{this.props.movies}</div>;
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((movie) => (
+          <MovieCard key={ movie.title } movie={ movies } />
+        ))}
+      </div>
+    );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyLine: PropTypes.string,
+      imagePath: PropTypes.string,
+      rating: PropTypes.number,
+    }),
+  ).isRequired,
+};
+
+export default MovieList;
