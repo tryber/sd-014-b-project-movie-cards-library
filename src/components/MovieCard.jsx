e77cd4e35b03f17aabc0096dd57e5e23e9d18712
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-09-01 21:03:27 -0300

    erro de digitacao corrigido no moviecard 13:7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,13 +4,13 @@ import React, { Component } from 'react';
 export default class MovieCard extends Component {
   render() {
     const { movie: {
-      title, subtitile, storyline, imagePath, rating
+      title, subtitle, storyline, imagePath, rating
     } } = this.props
     return (
       <div>
         <img src={imagePath} alt={title} />
         <h4>{title}</h4>
-        <h5>{subtitile}</h5>
+        <h5>{subtitle}</h5>
         <p>{storyline}</p>
       </div>
     );
