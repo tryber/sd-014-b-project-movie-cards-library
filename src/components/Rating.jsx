7ca668b734570dd72958d24b597f6df80d676bcd
Daniel <daniel.acoelho@outlook.com>
2021-09-02 15:38:07 -0300

    Requisito 11

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
-    render() {
-      const { rating } = this.props;
-      return (
-          <section>{rating}</section>
-      );
-    }
+  render() {
+    const { rating } = this.props;
+    return (
+      <section>{rating}</section>
+    );
+  }
 }
 
 Rating.propTypes = {
-    rating: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
 };
 
 export default Rating;
