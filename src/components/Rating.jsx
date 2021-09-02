a37ce61adb74d703aaa62b8a2d061baad77a0f3a
Victoria Lorena <victorialorenaf.gomes@gmail.com>
2021-09-01 23:40:34 -0300

    Item 12

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,24 @@
 // implement Rating component here
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      <section />
+      <section className="rating">
+        {rating}
+      </section>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  rating: null,
+};
 
 export default Rating;
