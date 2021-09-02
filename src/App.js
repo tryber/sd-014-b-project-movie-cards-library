bf5cd4bebe2a670eecb6f2355babb1d73b9a1f5e
Anderson Leite <andersonleite.dev@gmail.com>
2021-09-01 22:27:21 -0300

    Req 15

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 import MovieList from './components/MovieList';
 
 import data from './data';
@@ -7,6 +8,7 @@ import data from './data';
 function App() {
   return (
     <div className="App">
+      <Header />
       <MovieList movies={ data } />
     </div>
   );
