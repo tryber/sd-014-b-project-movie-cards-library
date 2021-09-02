e246f3de035355c18c6ff3756e82c45ca4fef66d
Thomas Ferreira <weltonthomasferreira@outlook.com>
2021-09-01 22:27:30 -0300

    Adiciona classes para implemntar o css

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movies={ data } />
+      <div className="movie-list">
+        <MovieList movies={ data } />
+      </div>
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,12 +8,20 @@ class MovieCard extends React.Component {
       movie: { title, subtitle, storyline, rating, imagePath },
     } = this.props;
     return (
-      <div>
-        <img src={ imagePath } alt="Imagem do filme" />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        <Rating rating={ rating } />
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <img
+            src={ imagePath }
+            alt="Imagem do filme"
+            className="movie-card-image"
+          />
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <div className="movie-card-rating">
+            <Rating rating={ rating } />
+          </div>
+        </div>
       </div>
     );
   }
