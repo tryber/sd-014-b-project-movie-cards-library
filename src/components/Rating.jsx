37840f433c7b4c2b0d2de603037cbc8db0bd7896
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2021-09-02 12:12:01 -0300

    estilizacao da pagina

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -6,7 +6,7 @@ class Rating extends Component {
   render() {
     const { rating } = this.props;
     return (
-      <div>
+      <div className="movie-card-rating">
         <p className="rating">{rating}</p>
       </div>
     );
