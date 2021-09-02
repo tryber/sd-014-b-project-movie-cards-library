f24c34b47d3d4b2c65582b6df0257501e99018a9
Leandro Nogueira <leandro2903.lan@gmail.com>
2021-09-01 21:47:18 -0300

    alterando o nome movie para adicionar a key ao invez de movies

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,7 +7,7 @@ class MovieList extends React.Component {
     const { movies } = this.props;
     return (
       <div>
-        { movies.map((filme) => <MovieCard key={ filme.title } movies={ filme } />)}
+        { movies.map((filme) => <MovieCard movie={ filme } key={ filme.title } />)}
       </div>
     );
   }
