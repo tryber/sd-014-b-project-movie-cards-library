59041024cb88055edebbebfdf15e050312a2dbdb
brenoMatias <matiasbreno19@gmail.com>
2021-09-02 11:21:13 -0300

    desctructuring movieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,16 +4,16 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component { // component estilo class
   render() { // fator fundamental na construção de classe component
-    const { title, subtitle, storyline, imagePath, rating } = this.props.movie; 
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props; // destructuring props assignment
 
     return (
-      <div className="movie">
+      <section className="movie">
         <img src={ imagePath } alt="test" />
         <h4> { title } </h4>
         <h5> { subtitle } </h5>
         <p> { storyline } </p>
         <Rating rating={ rating } />
-      </div>
+      </section>
     );
   }
 }
