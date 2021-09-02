180f03651a5239002b7dc9ec96dae66b9d088e0f
Ana-Alice-Honorio <ana.alice.honorio@gmail.com>
2021-09-02 19:04:05 -0300

    Parte funcional pronta

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
-import Rating from './components/Rating';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList />
-      <Rating />
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <MovieList movies={ movies } />
     </div>
   );
 }
