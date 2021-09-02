9e3d52a53d4bc0c13f906b1e34f4be39a9346445
dhenycruz <dheniarley.ds@gmail.com>
2021-09-02 16:46:16 -0300

    Aplicando classes e finalizando projeto.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
+import '../App.css';
 
 class MovieCard extends React.Component {
   render() {
     const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
 
     return (
-      <div>
-        <img src={ imagePath } alt={ `Imagem do filme ${title}` } />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
-        <Rating rating={ rating } />
+      <div className="movie-card">
+        <img src={ imagePath } alt={ `Imagem: ${title}` } className="movie-card-image" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className=".movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+          <Rating rating={ rating } />
+        </div>
       </div>
     );
   }
