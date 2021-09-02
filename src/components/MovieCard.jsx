82dd8565c8552e0ec7878fd5902c24ca64cc53f9
Rodrigo Cavalheiro <oldcavalheiro@gmail.com>
2021-09-01 21:20:47 -0300

    criado componente Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieList extends React.Component {
   render() {
@@ -10,7 +11,7 @@ class MovieList extends React.Component {
           <h4>{ title }</h4>
           <h5>{ subtitle }</h5>
           <p>{ storyline }</p>
-          { rating }
+          <Rating rating={ rating } />
         </div>
         <img src={ imagePath } alt={ `magem do filme ${title}` } />
       </section>
