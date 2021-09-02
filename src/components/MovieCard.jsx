6ba23f418fb3edda185df96145de807cb4de17d2
natalia <mellomartins.naty@gmail.com>
2021-09-01 21:03:18 -0300

    correção para o css no elemento Rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,19 +9,18 @@ class MovieCard extends React.Component {
     } = this.props;
     return (
       <section className="movie-card">
-        <div className="movie-card-body">
-          <img src={ imagePath } alt="Capa do Filme" className="movie-card-image" />
+        <img src={ imagePath } alt="Capa do Filme" className="movie-card-image" />
+        <div className="movie-card-body">          
           <h4 className="movie-card-title">{title}</h4>
           <h5 className="movie-card-subtitle">{subtitle}</h5>
           <p className="movie-card-storyline">{storyline}</p>
-          <Rating rating={ rating } />
-        </div>
+          </div>          
+        <Rating rating={ rating } />      
       </section>
     );
   }
 }
 
-// Dúvida em relação a escrita dos propTypes sanada na thread do Slack do aluno Esdras Oliveira, onde ele mostrou seu código.
 MovieCard.propTypes = {
   title: PropTypes.String,
   subtitle: PropTypes.String,
@@ -31,3 +30,7 @@ MovieCard.propTypes = {
 }.isRequired;
 
 export default MovieCard;
+
+// Referências:
+// 1. Dúvida em relação a escrita dos propTypes sanada na thread do Slack do aluno Esdras Oliveira, onde ele mostrou seu código.
+// 2. Tive ajuda do Rodolfo Braga e do Wellington Fonseca na correção da section para incluir corretamente as classes do css.
