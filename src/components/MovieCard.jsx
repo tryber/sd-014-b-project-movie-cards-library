842ad2d043b88e9fc11048631c52a0731d9a483a
icaroemanuel <icaro.emanuel@outlook.com>
2021-09-01 23:08:44 -0300

    Estilizei a seção de cards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -10,11 +10,13 @@ class MovieCard extends React.Component {
     } = this.props;
 
     return (
-      <section>
-        <img src={ imagePath } alt={ title } />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
+      <section className="movie-card">
+        <img src={ imagePath } alt={ title } className="movie-card-image" />
+        <article className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+        </article>
         <Rating rating={ rating } />
       </section>
     );
