9103b745248929d16783dbb50b6a6a3e023e5f3d
Paulo França <paulovinicius.franca@gmail.com>
2021-09-02 17:15:29 -0300

    Requisito 15

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <p>hora de codar</p>
     </div>
   );
