1106c6007f0aa4e0aef348a98a4169bba1a16b90
Cecilia Cacais <ceci.cacais@gmail.com>
2021-09-02 12:36:10 -0300

    Renderizando os componentes de moviecard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { title, subtitle } = this.props;
     return (
-      <section>Filmes card</section>
+      <section>
+        { title }
+        { subtitle }
+      </section>
     );
   }
 }
+
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+};
+
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { filmes } = this.props;
     return (
       <main>
-        <MovieCard />
+        { filmes.map((filme) => (
+          <MovieCard key={ filme.title } subtitle={ filme.subtitle } />
+        ))}
       </main>
     );
   }
 }
+
+MovieList.propTypes = {
+  filmes: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+    }),
+  ).isRequired,
+};
 
 export default MovieList;
