fad0efda1735355a7ed979347ff3d915313facfc
Guilherme Neri <guinsneri@gmail.com>
2021-09-03 13:20:22 -0300

    Desafio 1 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+// import data from './data';
 
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      hello world
+      <Header />
     </div>
   );
 }
