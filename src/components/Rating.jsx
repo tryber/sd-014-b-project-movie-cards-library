b0b85cd062586004002abc234e5b7f1ddfb69a58
Matheus Silveira <matheuspmsilveira@gmail.com>
2021-09-03 14:53:41 -0300

    refac: rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -7,9 +7,7 @@ class Rating extends Component {
     const { rating } = this.props;
     return (
       <div className="movie-card-rating">
-        <p className="rating">
-          {rating}
-        </p>
+        <div className="rating">{rating}</div>
       </div>
     );
   }
