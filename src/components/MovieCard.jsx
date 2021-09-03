89901bf90328c1bb05181cc1eb1d3d47bcade457
Renata <renatateixeira.est@gmail.com>
2021-09-03 14:02:54 -0300

    Requisito 3 e 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,17 @@
 // implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movieInfo } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movieInfo;
+    return (title, subtitle, storyline, imagePath, rating);
+  }
+}
+
+MovieCard.propTypes = {
+  movieInfo: PropTypes.string.isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,34 @@
 // implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    const listOfMovies = movies.map(
+      (movie, index) => <MovieCard key={ index } movieInfo={ movie } />,
+    );
+
+    return (
+      <ul>
+        <li>
+          { listOfMovies }
+        </li>
+      </ul>);
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+export default MovieList;
