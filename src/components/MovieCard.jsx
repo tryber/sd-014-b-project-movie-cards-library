0c01c83006c49e8f6bc2b7b394b6dc1e8ffd2c82
Bruno Trindade <brunotrndd@gmail.com>
2021-09-04 13:50:54 -0300

    requisito 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
-      <div />
+      <section
+        title={ movie.title }
+        subtitle={ movie.subtitle }
+        storyline={ movie.storyline }
+        imagePath={ movie.imagePath }
+        rating={ movie.rating }
+      />
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.string.isRequired,
+};
 
 export default MovieCard;
