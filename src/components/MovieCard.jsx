e577ede872a9f179cf72fd9de29a864a0fb827db
Bruno Trindade <brunotrndd@gmail.com>
2021-09-04 15:11:25 -0300

    ajustes css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,19 +6,21 @@ class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-      <section
-        title={ movie.title }
-        subtitle={ movie.subtitle }
-        storyline={ movie.storyline }
-        imagePath={ movie.imagePath }
-        rating={ movie.rating }
-        className="movie-card"
-      >
-        <img src={ movie.imagePath } alt={ movie.title } />
-        <h4 className="movie-card-title ">{ movie.title }</h4>
-        <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
-        <p className="movie-card-storyline">{ movie.storyline }</p>
-        <Rating rating={ movie.rating } className="movie-card-rating" />
+      <section>
+        <div
+          title={ movie.title }
+          subtitle={ movie.subtitle }
+          storyline={ movie.storyline }
+          imagePath={ movie.imagePath }
+          rating={ movie.rating }
+          className="movie-card"
+        >
+          <img src={ movie.imagePath } alt={ movie.title } />
+          <h4 className="movie-card-title ">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
+          <span className="movie-card-rating"><Rating rating={ movie.rating } /></span>
+        </div>
       </section>
     );
   }
