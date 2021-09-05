796b0afdb6d35812eb1b5d38aeab63acc519600b
vinicius-mns <vinicius_mns@hotmail.com>
2021-09-05 15:18:27 -0300

    fiz tudo de uma vez usando como referencia o codigo do levi

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+// feito analisando o código do Levi https://github.com/tryber/sd-014-b-project-movie-cards-library/tree/levi-manoel-movie-cards-library-project
 class MovieList extends React.Component {
   render() {
-    return <div>vazio</div>;
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+      </div>
+    );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,17 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+// feito analisando o código do Levi https://github.com/tryber/sd-014-b-project-movie-cards-library/tree/levi-manoel-movie-cards-library-project
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
+    return (
+      <div className="rating">{ rating }</div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;
