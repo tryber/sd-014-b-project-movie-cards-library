74fd336ef0ee679dd321594b929ba479498f6307
Juan Formoso <trajano.juan@gmail.com>
2021-09-04 23:56:21 -0400

    Não sei mais o que estou fazendo

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function MovieCard() {
-  return (
-    <div>
-      <span>Test</span>
-    </div>
-  );
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+    return (
+      <div>
+        <span>ABC</span>
+        <img src={ imagePath } alt={ title } />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <p>{rating}</p>
+      </div>
+    );
+  }
 }
-
+MovieCard.propTypes = {
+  movie: {
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }.isRequired,
+};
 export default MovieCard;
