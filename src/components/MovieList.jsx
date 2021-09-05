7d8d54b0054069f09162c653d5f73aa8fefcf043
Sergio_Rodrigues <rodrigues.sergiofilho@gmail.com>
2021-09-05 17:40:32 -0300

    Todos os requisitos :rocket:

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -7,7 +7,7 @@ class MovieList extends React.Component {
     const { movies } = this.props;
     return (
       <section>
-        {movies.map((element) => <MovieCard key={ element.title } movie={ movies } />)}
+        {movies.map((element) => <MovieCard key={ element.title } movie={ element } />)}
       </section>
     );
   }
