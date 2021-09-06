fba4a8045bbc6cbd4d61ca7193be38535cdac82f
Diogo Fiuza <difipalm@gmail.com>
2021-09-06 17:00:46 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      { movies.map((movie) => (
+        <MovieList movies={ movie } />
+      ))}
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const { title, imagePath, subtitle, storyline } = this.props.movies;
+    return (<div>
+      <img src= { imagePath }/>
+      <h1>{ title }</h1>
+    </div>);
+  }
+}
+
+export default MovieList;
