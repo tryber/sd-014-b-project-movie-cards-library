c8caf02d0c07742f5f5c096f1ce870d8e88f114e
= <luz.matheus11@gmail.com>
2021-09-06 18:00:44 -0300

    Corrigindo o lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,7 +9,7 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, imagePath, rating } = movie;
 
     return (
-      <div className="movie-card">
+      <div className="movie-card-body">
         <h4 className="movie-card-title">{title}</h4>
         <h5 className="movie-card-subtitle">{subtitle}</h5>
         <p className="movie-card-subtitle">{storyline}</p>
