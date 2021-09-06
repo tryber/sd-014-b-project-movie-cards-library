a962235548b9610405c5cff3299212414c090d87
Danilo Duarte <daniloformer@gmail.com>
2021-09-06 20:38:53 -0300

    Requisitos 11 ao 14

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,11 +1,21 @@
-// import React from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-// class Rating extends React.Component {
-//   render() {
-//     const { movie } = this.props;
+class Rating extends React.Component {
+  render() {
+    const { rating } = this.props;
 
-//     return (
-//       <section>      </section>
-//     );
-//   }
-// };
+    return (
+      <section className="rating">
+        { rating }
+      </section>
+    );
+  }
+}
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+Rating.defaultProps = {
+  rating: 0,
+};
+export default Rating;
