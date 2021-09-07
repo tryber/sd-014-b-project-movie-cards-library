4925016017bf4dbd86b3c7e299e15b2ae57dd908
Everson Elias Gonçalves de Oliveira <eversoneliasg@gmail.com>
2021-09-06 23:44:04 -0300

    Demais requisitos: segunda submissão

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,12 +12,13 @@ class MovieCard extends React.Component {
         storyline={ movie.storyline }
         imagePath={ movie.imagePath }
         rating={ movie.rating }
-        className="movie-card">
-          <img src={ movie.imagePath } alt={ movie.title } />
-          <h4 className="movie-card-title ">{ movie.title }</h4>
-          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
-          <p className="movie-card-storyline">{ movie.storyline }</p>
-          <Rating rating={ movie.rating } className="movie-card-rating" />
+        className="movie-card"
+      >
+        <img src={ movie.imagePath } alt={ movie.title } />
+        <h4 className="movie-card-title ">{ movie.title }</h4>
+        <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+        <p className="movie-card-storyline">{ movie.storyline }</p>
+        <Rating rating={ movie.rating } className="movie-card-rating" />
       </section>
     );
   }
