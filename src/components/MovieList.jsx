56e28dc2fa11bb7c45ef699f9e6ec7c0864e3d20
Everson Elias Gonçalves de Oliveira <eversoneliasg@gmail.com>
2021-09-06 22:53:58 -0300

    Requisito n. 3: criação do componente <MovieList />

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <main>{ movies }</main>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.string.isRequired,
+};
+
+export default MovieList;
