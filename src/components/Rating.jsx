bca00358d5fe973109c3c89c191e7578527f8e62
Eicke Yumoto <eickeyumoto@gmail.com>
2021-09-08 13:56:37 -0300

    correçao

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Rating extends React.component {
+class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
