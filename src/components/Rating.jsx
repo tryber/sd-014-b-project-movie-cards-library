7079d8b1eb28a2d2c71c8db07f8b67e505720326
Paola Gay <paolagaydossantos@gmail.com>
2021-09-07 21:43:07 -0300

    Req Rating: 11 and 12

---
+++ src/components/Rating.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { rating }= this.props;
+    return (
+      <div className='rating'>
+      {rating}</div>
+    );
+  }
+}
+
+Rating.propTypes={
+rating: PropTypes.string,
+}.isRequired;
+
+export default Rating;
