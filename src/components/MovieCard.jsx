d98c92e50e447e7286e7ce8218ffb69f1dc22005
viniciussantana <viniciusantana@yahoo.com.br>
2021-09-09 18:27:16 -0300

    Adiciona classes a header e moviecard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,11 +8,11 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
-      <section>
-        <img src={ imagePath } alt="" />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
+      <section className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt="" />
+        <h4 className="movie-card-title">{ title }</h4>
+        <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <p className="movie-card-storyline">{ storyline }</p>
         <Rating rating={ rating } />
       </section>
     );
