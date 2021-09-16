8a818c167b74eaec37010d3523e77998faf5247e
Victor Martins <victor.martins.duarte@gmail.com>
2021-09-16 00:27:14 -0300

    Requirement 8

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,9 +3,12 @@ import PropTypes from 'prop-types';
 
 class MovieCard extends Component {
   render() {
-    const { movie: title, subtitle, storyline, imagePath, rating } = this.props;
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
     return (
-      <img src={ imagePath } alt="" />
+      <section>
+        <img src={ imagePath } alt="" />
+        <h4>{ title }</h4>
+      </section>
     );
   }
 }
