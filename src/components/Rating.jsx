d384c3fbaee6fe65bf53c0b2f8837e773a33024d
IgorFellows <igor.fpp96@gmail.com>
2021-09-16 15:22:31 -0300

    Acabando o projeto

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,3 +1,5 @@
+/* Por algum motivo, meu Rating não estava funcionando, então peguei o componente do Tiago Ponce, no github dele. https://github.com/tryber/sd-014-b-project-movie-cards-library/pull/111/files
+ */
 import React from 'react';
 import PropTypes from 'prop-types';
 
