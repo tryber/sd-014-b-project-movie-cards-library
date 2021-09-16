979a973b2270320d3f3cb94a33281aab580379c5
IgorFellows <igor.fpp96@gmail.com>
2021-09-16 15:13:26 -0300

    testando

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ButtonWatch from './ButtonWatch';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, imagePath, subtitle, storyline, rating, link } = movie;
+    const { title, imagePath, subtitle, storyline, rating } = movie;
     return (
       <section className="movie-card">
         <img className="movie-card-image" src={ imagePath } alt={ title } />
@@ -15,7 +14,6 @@ class MovieCard extends React.Component {
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
           <div className="little-menu">
-            <ButtonWatch weblink={ link } />
             <div className="movie-card-rating">
               <h6>Classificação:</h6>
               <Rating rating={ rating } />
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,19 +5,13 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <section>
-        <p className="Rating">{ rating }</p>
-      </section>
+      <p className="rating">{rating}</p>
     );
   }
 }
 
 Rating.propTypes = {
-  rating: PropTypes.number,
-};
-
-Rating.defaultProps = {
-  rating: 70,
+  rating: PropTypes.number.isRequired,
 };
 
 export default Rating;
