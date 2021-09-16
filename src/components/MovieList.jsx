bd8108320fd27b668202414899ec6a3e23969a44
Victor Martins <victor.martins.duarte@gmail.com>
2021-09-15 21:25:31 -0300

    Requirement 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,27 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <main>
+        { movies.map((movie) => movie.title) }
+      </main>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape(
+    {
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    },
+  )).isRequired,
+};
+
+export default MovieList;
