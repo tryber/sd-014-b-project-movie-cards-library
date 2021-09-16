47353becf6a28bfa968ef622ccf5118a4965d44b
Vitor Luquini <vitorluquini2013@hotmail.com.br>
2021-09-16 12:40:24 -0300

    MovieList Comp

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section>
+        {movies.map((obj) => <MovieCard movie={ obj } key={ obj.title } />)}
+      </section>
+    );
+  }
+}
+
+export default MovieList;
