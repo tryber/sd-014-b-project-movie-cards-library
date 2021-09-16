6c9f351360265ac8ba19fc8a736e96f408a59dd1
Vitor Luquini <vitorluquini2013@hotmail.com.br>
2021-09-16 12:42:26 -0300

    MovieCard Comp

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <MovieList />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <MovieList />
+      </div>
+    );
+  }
 }
 
 export default App;
