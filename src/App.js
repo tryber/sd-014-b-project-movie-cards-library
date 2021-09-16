32045147ea1dafef3aa3ae5557aa708b2abeab4c
Vitor Luquini <vitorluquini2013@hotmail.com.br>
2021-09-16 12:54:21 -0300

    Fix proptypes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     return (
       <div className="App">
         <Header />
-        <MovieList />
+        <MovieList movies={ movies } />
       </div>
     );
   }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
       <section>
         <img src={ imagePath } alt={ title } />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        <Rating rating={ rating } className="rating" />
+        <Rating rating={ rating } />
       </section>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.exact({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
