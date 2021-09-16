7047287091373d3e2f75867adc9f6ad7de1a624c
Victor Martins <victor.martins.duarte@gmail.com>
2021-09-16 00:47:07 -0300

    Requirement 13

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -11,15 +11,7 @@ class Rating extends Component {
 }
 
 Rating.propTypes = {
-  movie: PropTypes.arrayOf(PropTypes.shape(
-    {
-      title: PropTypes.string,
-      subtitle: PropTypes.string,
-      storyline: PropTypes.string,
-      rating: PropTypes.number,
-      imagePath: PropTypes.string,
-    },
-  )).isRequired,
-};
+  rating: PropTypes.number,
+}.isRequired;
 
 export default Rating;
