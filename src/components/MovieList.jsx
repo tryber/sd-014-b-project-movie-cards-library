d60bad8751b2d51fe3fed30ac1fd555b95f572ef
Rodrigo F de Avila <rodrigoferraodeavila@gmail.com>
2021-09-18 18:08:49 -0300

    bomba

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
 // implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return movies.map((obj) => <MovieCard key={ obj.title } movie={ obj } />);
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;
